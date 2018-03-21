2b2e35508d22db26a3e76793a5868d3d30672970
Ahmad Ibrahim <ahmadmuhamo@gmail.com>
2018-03-21 18:31:28 +0200

    Added Event Listeners

--- js/app.js
+++ js/app.js
@@ -1,11 +1,29 @@
-var budgetController = ( () => {
+/**Budget Controller */
+let budgetController = (() => {
 
 })();
 
-var uiController = (() => {
+/**UI Controller */
+let uiController = (() => {
 
 })();
 
-var controller = ((bc, uc) => {
+/**APP Controller */
+let controller = ((bc, uc) => {
+
+    let addItem = () => {
+        // 1- Get input Data
+        // 2- Add the item to the Budget Controller
+        // 3- Add the new Item to the UI
+        // 4- Calculate the Budget
+        // 5- Display the Budget on the UI
+    };
     
+    document.querySelector('.add__btn').addEventListener('click',addItem);
+
+    document.addEventListener('keypress',(event) => {
+        if (event.keyCode === 13 || event.which === 13) {
+            addItem();
+        }
+    });
 })(budgetController, uiController);
